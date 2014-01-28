34dc7344c4dd7a6546bb41e9c8b7267431934e1d
Dominic McGinnis <dominicmcginnis@gmail.com>
2014-01-28 15:37:42 -0800

    Added remaining routes

--- server.js
+++ server.js
@@ -10,8 +10,18 @@ app.configure(function() {
 	app.set('port', 8080);
 });
 
+//Routes
 app.get('/', function(req, res){
 	res.render("index.html");
+});
+app.get('/team', function(req, res){
+	res.render("team.html");
+});
+app.get('/rankings', function(req, res){
+	res.render("matchups.html");
+});
+app.get('/standings', function(req, res){
+	res.render("rankings.html");
 });
 
 http.createServer(app).listen(app.get('port'), function(){
